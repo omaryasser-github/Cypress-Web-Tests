8d5dffacb44d2d4ced0e7e389b89bc4cebaa2d27
Omar Yasser <anaesthesiayassermadi717@gmail.com>
2026-05-15 19:33:10 +0300

    Update: Cypress configuration to set viewport size and adjust file change settings

--- cypress.config.js
+++ cypress.config.js
@@ -7,5 +7,12 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    // igonre some comments 
+    chromeWebSecurity: false,
+    watchForFileChanges: false,
+
+    // Take full page size 
+    viewportWidth: 1920,
+    viewportHeight: 1080,
   },
 });
